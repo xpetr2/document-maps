2b1202f673d329778e7a9d9f36ad943cd75c7a53
Michal Petr <tornith@gmail.com>
2021-05-11 02:18:41 +0200

    Renaming of project

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/bakalarka'),
+      dir: require('path').join(__dirname, './coverage/document-maps'),
       subdir: '.',
       reporters: [
         { type: 'html' },
